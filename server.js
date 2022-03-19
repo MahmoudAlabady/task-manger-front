a6921a3c3235b4a9ee1497599e9cd12fc0a4cabe
MahmoudAlabady <36644832+MahmoudAlabady@users.noreply.github.com>
2022-03-20 00:44:19 +0200

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require('express')
-const app = express()
+const express= require('express')
+const app= express()
 app.use(express.static(__dirname + '/dist/task-manger'))
+app.get('*',(req,res)=>{
+    	res.sendFile('index.html',{root:'dist/task-manger/'})
+ })
 app.listen(process.env.PORT)
